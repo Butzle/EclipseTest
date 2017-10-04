72c211772caea6102cb126507ca93f48f88d068d
jo schimberg <jo.schimberg.001@student.uni.lu>
2017-10-04 23:02:20 +0200

    no message

--- Fibonacci/src/talkwalker/schimberg/jo/Fibonacci.java
+++ Fibonacci/src/talkwalker/schimberg/jo/Fibonacci.java
@@ -15,7 +15,7 @@ public class Fibonacci {
 		fibonacciNumbers.add(1);
 		fibonacciNumbers.add(2);
 		
-		System.out.println("test");
+		System.out.println("test200292");
 		
 		int fibonacciSum = 0;
 		
